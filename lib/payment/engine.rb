f3251d6f956af45994b6ed6dd9391c94c57ea258
Richard Huang <flyerhzm@gmail.com>
2012-05-04 10:56:00 +0800

    alipay and paypal configuration

--- lib/payment/engine.rb
+++ lib/payment/engine.rb
@@ -1,5 +1,24 @@
 module Payment
   class Engine < ::Rails::Engine
     isolate_namespace Payment
+
+    initializer "payment_engine.load_configs" do |app|
+      CONFIGS = YAML.load_file(Rails.root.join("config/payment.yml"))[Rails.env]
+      paypal_config = CONFIGS["paypal"]
+
+      ActiveMerchant::Billing::Base.mode = paypal_config["mode"]
+      ::GATEWAY = ActiveMerchant::Billing::PaypalGateway.new(
+        :login => paypal_config["login"],
+        :password => paypal_config["password"],
+        :signature => paypal_config["signature"]
+      )
+
+      ActiveMerchant::Billing::Integrations::Alipay::KEY = CONFIGS["alipay"]["key"]
+
+      require 'active_merchant'
+      require 'active_merchant/billing/integrations/action_view_helper'
+
+      ActionView::Base.send(:include, ActiveMerchant::Billing::Integrations::ActionViewHelper)
+    end
   end
 end
