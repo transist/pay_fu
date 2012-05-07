7ee5aac05763dff993b7cc0a236ca50b987e2a98
Richard Huang <flyerhzm@gmail.com>
2012-05-07 21:04:04 +0800

    generate payment:install

--- lib/generators/payment/install_generator.rb
+++ lib/generators/payment/install_generator.rb
@@ -1,16 +1,19 @@
 require 'rails/generators'
+require File.expand_path('../utils', __FILE__)
 
-module RailsAdmin
+module Payment
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path("../templates", __FILE__)
     include Rails::Generators::Migration
+    include Generators::Utils::InstanceMethods
+    extend Generators::Utils::ClassMethods
 
     desc "Payment installation generator"
     def install
-      display "Adding a migration..."
-      migration_template 'migration.rb', 'db/migrate/create_rails_admin_histories_table.rb' rescue display $!.message
+      migration_template 'migration.rb', 'db/migrate/create_payment_transactions.rb' rescue display $!.message
       route("mount Payment::Engine => '/payment', :as => 'payment'")
-      display "Job's done: migrate, start your server and visit '/#{namespace}'!", :blueend
+      template "config.yml", "config/payment.yml"
+      display "Payment engine is installed."
     end
   end
 end
--- lib/payment/engine.rb
+++ lib/payment/engine.rb
@@ -3,22 +3,34 @@ module Payment
     isolate_namespace Payment
 
     initializer "payment_engine.load_configs" do |app|
-      CONFIGS = YAML.load_file(Rails.root.join("config/payment.yml"))[Rails.env]
-      paypal_config = CONFIGS["paypal"]
+      config_file = Rails.root.join("config/payment.yaml")
+      if config_file.file?
+        CONFIGS = YAML.load_file(config_file)[Rails.env]
+        paypal_config = CONFIGS["paypal"]
 
-      ActiveMerchant::Billing::Base.mode = paypal_config["mode"]
-      ::GATEWAY = ActiveMerchant::Billing::PaypalGateway.new(
-        :login => paypal_config["login"],
-        :password => paypal_config["password"],
-        :signature => paypal_config["signature"]
-      )
+        ActiveMerchant::Billing::Base.mode = paypal_config["mode"]
+        ::GATEWAY = ActiveMerchant::Billing::PaypalGateway.new(
+          :login => paypal_config["login"],
+          :password => paypal_config["password"],
+          :signature => paypal_config["signature"]
+        )
 
-      ActiveMerchant::Billing::Integrations::Alipay::KEY = CONFIGS["alipay"]["key"]
+        ActiveMerchant::Billing::Integrations::Alipay::KEY = CONFIGS["alipay"]["key"]
 
-      require 'active_merchant'
-      require 'active_merchant/billing/integrations/action_view_helper'
+        require 'active_merchant'
+        require 'active_merchant/billing/integrations/action_view_helper'
 
-      ActionView::Base.send(:include, ActiveMerchant::Billing::Integrations::ActionViewHelper)
+        ActionView::Base.send(:include, ActiveMerchant::Billing::Integrations::ActionViewHelper)
+      end
+    end
+
+    initializer "warn when configuration is missing" do
+      config.after_initialize do
+        unless Rails.root.join("config/payment.yml").file?
+          puts "\nPayment config not found. Create a config file at: config/payment.yml"
+          puts "to generate one run: rails generate payment:config\n\n"
+        end
+      end
     end
   end
 end
