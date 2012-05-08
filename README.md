475075244d76fc2e6e5e64e788dec52b0ab5e75d
Richard Huang <flyerhzm@gmail.com>
2012-05-08 21:36:26 +0800

    add readme and pass hash to redirect_to_paypal_gateway helper method

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 payment
 =======
+
+1. add to Gemfile
+
+    gem "payment", :git => "git@github.com:transist/payment.git"
+
+2. bundle install
+
+3. rails generate payment:install
--- app/helpers/payment/paypal_helper.rb
+++ app/helpers/payment/paypal_helper.rb
@@ -1,7 +1,7 @@
 module Payment
   module PaypalHelper
-    def redirect_to_paypal_gateway(item_name, amount)
-      redirect_to URI.encode("https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=#{Payment::CONFIGS["paypal"]["email"]}&currenct_code=USD&item_name=#{item_name}&amount=#{amount}")
+    def redirect_to_paypal_gateway(options={})
+      redirect_to URI.encode("https://www.paypal.com/cgi-bin/webscr?cmd=_ext-enter&redirect_cmd=_xclick&charset=utf-8&business=#{Payment::CONFIGS["paypal"]["email"]}&currenct_code=USD&item_name=#{options[:item_name]}&amount=#{options[:amount]}") and return
     end
   end
 end
