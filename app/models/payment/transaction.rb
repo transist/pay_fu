b8e925309a145881acc0c3fee505e1a156040419
Richard Huang <flyerhzm@gmail.com>
2012-05-09 20:54:29 +0800

    udpate Payment::Transaction.attr_accessible

--- app/models/payment/transaction.rb
+++ app/models/payment/transaction.rb
@@ -1,5 +1,5 @@
 module Payment
   class Transaction < ActiveRecord::Base
-    attr_accessible :mc_currency, :mc_fee, :mc_gross, :payer_email, :payer_id, :payment_date, :payment_status, :raw_post, :txn_id, :txn_type, :type
+    attr_accessible :transaction_id, :transaction_type, :payment_status, :payment_date, :gross, :raw_post, :type
   end
 end
