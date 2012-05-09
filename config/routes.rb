95737014bb76820e3815b8bf63dff7398c095a19
Richard Huang <flyerhzm@gmail.com>
2012-05-09 08:09:56 +0800

    define notify urls in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 Payment::Engine.routes.draw do
+  match 'payment/paypal_transactions/notify' => 'payment/paypal_transactions#notify'
+  match 'payment/alipay_transactions/notify' => 'payment/alipay_transactions#notify'
 end
