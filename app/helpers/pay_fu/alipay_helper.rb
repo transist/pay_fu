17d1f271a52f9cff58a23eaf904ef38f1ff80934
Rainux Luo <rainux@gmail.com>
2012-07-26 12:20:03 +0800

    Extract private method #query_params

--- app/helpers/pay_fu/alipay_helper.rb
+++ app/helpers/pay_fu/alipay_helper.rb
@@ -3,7 +3,16 @@ require 'digest/md5'
 module PayFu
   module AlipayHelper
     def redirect_to_alipay_gateway(options={})
-      query_string = {
+      query_string = query_params(options).sort.map { |key, value| "#{key}=#{CGI.unescape(value)}" }.join("&")
+      sign = Digest::MD5.hexdigest(query_string + ActiveMerchant::Billing::Integrations::Alipay::KEY)
+      query_string += "&sign=#{sign}&sign_type=MD5"
+
+      redirect_to "https://www.alipay.com/cooperate/gateway.do?" + query_string
+    end
+
+    private
+    def query_params(options)
+      {
         :partner => ActiveMerchant::Billing::Integrations::Alipay::ACCOUNT,
         :out_trade_no => options[:out_trade_no],
         :total_fee => options[:amount],
@@ -14,11 +23,7 @@ module PayFu
         :service => ActiveMerchant::Billing::Integrations::Alipay::Helper::CREATE_DIRECT_PAY_BY_USER,
         :payment_type => "1",
         :subject => options[:subject]
-      }.sort.map { |key, value| "#{key}=#{CGI.unescape(value)}" }.join("&")
-      sign = Digest::MD5.hexdigest(query_string + ActiveMerchant::Billing::Integrations::Alipay::KEY)
-      query_string += "&sign=#{sign}&sign_type=MD5"
-
-      redirect_to "https://www.alipay.com/cooperate/gateway.do?" + query_string
+      }
     end
   end
 end
