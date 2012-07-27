300db75325c7d1a426d2cfc91c09537cd8015748
Rainux Luo <rainux@gmail.com>
2012-07-27 15:13:16 +0800

    Fix #raw_post typo

--- app/controllers/pay_fu/alipay_transactions_controller.rb
+++ app/controllers/pay_fu/alipay_transactions_controller.rb
@@ -3,7 +3,7 @@ module PayFu
     include ActiveMerchant::Billing::Integrations
 
     def notify
-      notify = Alipay::Notification.new(request.new_post)
+      notify = Alipay::Notification.new(request.raw_post)
       if notify.acknowledge
         if transaction = PayFu::AlipayTransaction.find_by_transaction_id(notify.trade_no)
           transaction.update_attributes(transaction_attributes(notify))
